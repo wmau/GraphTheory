9143f7d29c49ce0a22a774539cf1393a9cf37f3d
wmau <wmau@bu.edu>
2015-11-07 14:34:31 -0500

    Negate eigen centrality
    For some reason, the eigenvector centralities are negative.

--- parseCentrality.m
+++ parseCentrality.m
@@ -8,7 +8,7 @@ function cent = parseCentrality(sessionStruct,centralitytype)
     switch centralitytype
             case 'eigenvector'
                 load(fullfile(path,'Centralities.mat'),'eCent'); 
-                cent = eCent; 
+                cent = -eCent; 
             case 'betweenness'
                 load(fullfile(path,'Centralities.mat'),'betCent'); 
                 cent = betCent; 
