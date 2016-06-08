d4ddff1085f42e15bb2d26ac59902a868dd10adf
wmau <wmau@bu.edu>
2016-06-08 12:14:37 -0400

    edits

--- MakeGraphv3.m
+++ MakeGraphv3.m
@@ -26,98 +26,40 @@ function MakeGraphv3(md)
         
     %Preallocate connectivity matrix. 
     nNeurons = size(FT,1);
-    A = zeros(nNeurons); 
-    Ap = zeros(nNeurons); 
-    B = 200;
     
-%% Construct graph. 
+    %% Construct graph. 
     lagMat = cell(nNeurons); 
-    
-    %Null distribution for comparing empirical lags. 
-    null = cell(nNeurons);
     
     %For each lap...
     p = ProgressBar(nLaps);
+
     for l=1:nLaps
         %Get the onset times of each neuron. 
         onset = getFEpochs(FT(:,inds(l,1):inds(l,2)));
         
         %For each neuron...
-        for n=1:nNeurons
-            %Look at onset +/- window. 
-            wBack = onset{n}-nBins;
-            wForward = onset{n}+nBins; 
-            
+        for n=1:nNeurons      
             %Number of calcium events in this neuron on this lap. 
             nEpochs = length(onset{n}); 
             
             %For each calcium event...
             for e=1:nEpochs
-                b = wBack(e);       %Index of beginning of epoch minus lag. 
-                f = wForward(e);    %Index of end of epoch plus lag. 
-                
-                %Other neurons that fire within the window of neuron n. 
-                coincidentNeurons = find(cellfun(@any,cellfun(@(c) c>b & c<f, onset,'unif',0)));
-                coincidentNeurons(coincidentNeurons==n) = [];             %Remove self-coincidence.
-                
-                %For each other neuron...
+                coincidentNeurons = find(cell2mat(cellfun(@any,onset,'unif',0)));
+                coincidentNeurons(coincidentNeurons==n) = []; 
+
+        %For each other neuron...
                 for c=coincidentNeurons'
                     %Find the interval between neuron n and neuron c.
-                    lags = b+nBins-onset{c};
-                    lagMat{n,c} = [lagMat{n,c}, lags(lags<nBins & lags>-nBins)./20];
-                    
-                    for nn=1:B
-                        %Shuffle traces of neuron c in time then find
-                        %onset. Append a 0 to the end of the vector being
-                        %shuffled to prevent connecting transients at the
-                        %end and beginning of the epoch.
-                        shuffledOnset = getFEpochs(circshift(...
-                            [FT(c,inds(l,1):inds(l,2)) 0],...
-                            [0,randi([0,200])]));
-                        
-                        %Circularly permuting sometimes introduces new
-                        %epoch. Take the same number as the original.
-                        shuffledOnset = shuffledOnset{1}(1:length(onset{c}));                       
-                                                         
-                        %Lag between neuron n spike and neuron c spike
-                        %(shuffled).
-                        nullLag = b+nBins-shuffledOnset;
-                        null{n,c} = [null{n,c}, nullLag(nullLag<nBins & nullLag>-nBins)./20];
-                    end
-                   
-                end
+                    lags = onset{n}(e)-onset{c};
+ 
+                    lagMat{n,c} = [lagMat{n,c}, max(lags(lags>-nBins & lags<0))./20];           
+                end    
             end
-        end         
+        end 
         p.progress;
     end
-    p.stop;
+
+    graphDatav3.lagMat = lagMat;
     
-    %Perform pairwise comparisons between lag distributions for each neuron
-    %(in both directions). Convention: row connects to colum, so nn should
-    %fire first and lag between nn and cc should be negative. 
-    alpha = 0.05/(nNeurons-1);
-    p = ProgressBar(nNeurons);
-    for nn=1:nNeurons
-        for cc=1:nNeurons
-            [~,e,b] = histcounts(lagMat{nn,cc},'binwidth',0.5);
-            if ~isempty(lagMat{nn,cc}) && ...
-                    length(lagMat{nn,cc}) > 3 && ...
-                    e(mode(b)) < 0
-                [h,pval] = kstest2(lagMat{nn,cc},null{nn,cc},'alpha',alpha);
-                
-                Ap(nn,cc) = pval; 
-                A(nn,cc) = h;
-            end
-        end
-        p.progress;
-    end
-    p.stop;
-    
-    graphData.A = A; 
-    graphData.Ap = Ap; 
-    graphData.null = null;
-    graphData.lagMat = lagMat;
-    
-    save('Graph.mat','graphData','-v7.3');
-    
+    save('Graph.mat','graphDatav3','-v7.3');
 end
